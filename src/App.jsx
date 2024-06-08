4574a54bcdb4da902ef8596143ef65152d4e9535
Sam Sharma <samsharma.dev@gmail.com>
2024-06-08 08:49:53 -0700

    grabbed the url for openweather api

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './App.scss';
 import axios from 'axios';
 
 function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=6d7e49e3ac86846731af17a9cb61014d`;
+
   return (
     <div className='App'>
-      
+
     </div>
   );
 }
