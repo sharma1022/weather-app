44da7aaa1ed63523f69f83d74e9153e1fa1644ab
Sam Sharma <samsharma.dev@gmail.com>
2024-06-08 08:40:44 -0700

    initial commit with updated dependencies

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './App.scss';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className='App'>
+      
+    </div>
+  );
+}
+
+export default App;
